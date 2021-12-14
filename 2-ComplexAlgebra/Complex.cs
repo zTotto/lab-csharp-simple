d2365650083518eb34f04ae3a260c242c0ec57ce
Lorenzo Todisco <lorenzo.todisco@studio.unibo.it>
2021-12-14 18:51:36 +0100

    Added ex2

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,22 +1,51 @@
+using System;
+
 namespace ComplexAlgebra
 {
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
     ///
-    /// TODO: Model Complex numbers in an object-oriented way and implement this class.
-    /// TODO: In other words, you must provide a means for:
-    /// TODO: * instantiating complex numbers
-    /// TODO: * accessing a complex number's real, and imaginary parts
-    /// TODO: * accessing a complex number's modulus, and phase
-    /// TODO: * complementing a complex number
-    /// TODO: * summing up or subtracting two complex numbers
-    /// TODO: * representing a complex number as a string or the form Re +/- iIm
-    /// TODO:     - e.g. via the ToString() method
-    /// TODO: * checking whether two complex numbers are equal or not
-    /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
         // TODO: fill this class\
+        public double Real { get; }
+        public double Imaginary { get; }
+
+        public Complex(double real, double imag)
+        {
+            this.Real = real;
+            this.Imaginary = imag;
+        }
+
+        public double Modulus => Math.Sqrt(Real*Real + Imaginary*Imaginary);
+
+        public double Phase => Math.Atan2(Imaginary,Real);
+
+        public Complex Complement() => new Complex(Real, -Imaginary);
+
+        public Complex Plus(Complex c2) => new Complex(Real + c2.Real, Imaginary + c2.Imaginary);
+
+        public Complex Minus(Complex c2) => new Complex(Real - c2.Real, Imaginary - c2.Imaginary);
+
+        public override string ToString()
+        {
+            if (Real == 0 && Imaginary == 0)
+            {
+                return "0";
+            } else
+            return (Real == 0 ? "" : $"{Real}") + 
+                   (Imaginary == 0 ? "" : (Imaginary >= 0 ? $"+i{Imaginary}" : $"-i{-Imaginary}"));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Complex complex && Real == complex.Real && Imaginary == complex.Imaginary;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
